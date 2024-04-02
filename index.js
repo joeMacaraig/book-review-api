325ceb847eb0e9acbdf3cfb7293d7593ead8c24c
Joseph Macaraig <jbmacaraig14@gmail.com>
2024-04-02 14:19:08 -0700

    uploaded files
    finished the auth and all the routes. now i will create a controller folder to do the controllers of the routes so we turn the routes to async/await functions. also to use promises

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import session from "express-session";
+
 import { publicRoutes } from "./routes/public-users.js";
+import { authRouter } from "./routes/auth-users.js";
 
 dotenv.config();
 
@@ -11,15 +13,30 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(
-  "/customer",
   session({
     secret: "fingerprint_customer",
     resave: true,
     saveUninitialized: true,
   })
 );
-app.use("/customer/auth/*", function auth(req, res, next) {});
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if (req.session.authorization) {
+    const token = req.session.authorization["accessToken"];
+    jwt.verify(token, "access", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).send({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).send({ message: "User not Logged in" });
+  }
+});
+
 app.use(publicRoutes);
+app.use("/customer", authRouter);
 
 app.listen(PORT, () =>
   console.log(`📖 Book Review listening on port: ${PORT} 🚀`)
---
+++ models/users.js
@@ -0,0 +1,1 @@
+export const users = { 1: { username: "user1", password: "123abc" } };
